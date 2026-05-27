be07e94c178c36297c99fac9bfa6fef0ab6de879
Andreza Damasceno <151277972+andrwza@users.noreply.github.com>
2026-05-27 09:57:38 -0300

    Refactor fila.c to improve code structure

--- fila.c
+++ fila.c
@@ -1,66 +1,51 @@
 #include <stdio.h>
-
 #define TAM 5
 
 typedef struct {
-
     int dados[TAM];
     int inicio;
     int fim;
-
 } Fila;
 
 Fila fila;
 
 // Inicializa fila
 void inicializarFila() {
-
     fila.inicio = 0;
     fila.fim = -1;
 }
 
 // Verifica se fila está vazia
 int filaVazia() {
-
     return (fila.inicio > fila.fim);
 }
 
 // Adiciona elemento
 void enfileirar(int valor) {
-
     fila.fim++;
-
     fila.dados[fila.fim] = valor;
-
     printf("Valor %d inserido na fila\n", valor);
 }
 
 // Remove elemento
 int desenfileirar() {
-
     if(filaVazia()) {
-
         printf("Fila vazia\n");
-
         return -1;
     }
-
+    
     int valor = fila.dados[fila.inicio];
-
     fila.inicio++;
-
+    
     return valor;
 }
 
 // Exibe fila
 void exibirFila() {
-
     int i;
 
     printf("Fila: ");
-
     for(i = fila.inicio; i <= fila.fim; i++) {
-
         printf("%d ", fila.dados[i]);
     }
 
@@ -68,13 +53,11 @@ void exibirFila() {
 }
 
 int main() {
-
     inicializarFila();
-
     enfileirar(10);
     enfileirar(20);
     enfileirar(30);
-
+    
     exibirFila();
 
     printf("Removido: %d\n", desenfileirar());
